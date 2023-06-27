4e82338119536dc95b6ddd9ca1fa599d3883a745
Rule Timothy (CC/EMT2) <Timothy.Rule@de.bosch.com>
2023-06-27 15:59:07 +0200

    Add input signals for MStep developer interface.
    Signed-off-by: Rule Timothy (CC/EMT2) <Timothy.Rule@de.bosch.com>

--- dse/modelc/tools/mstep/mstep.c
+++ dse/modelc/tools/mstep/mstep.c
@@ -7,18 +7,74 @@
 #include <stdint.h>
 #include <errno.h>
 #include <dlfcn.h>
+#include <dse/clib/collections/hashmap.h>
+#include <dse/clib/util/yaml.h>
+#include <dse/modelc/schema.h>
 #include <dse/modelc/model.h>
 #include <dse/logger.h>
 
 
-#define STEP_SIZE 0.005
-#define END_TIME  3600
-#define STEPS     10
+#define STEP_SIZE     0.005
+#define END_TIME      3600
+#define STEPS         10
+
+
+#define UNUSED(x)     ((void)x)
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof(x[0]))
 
 
 static void marshal_to_signal_vectors(SignalVector* sv);
 static void marshal_from_signal_vectors(SignalVector* sv);
 static void print_signal_vectors(SignalVector* sv);
+
+
+/**
+ *  Input parser (YAML doc kind = "Input").
+ *
+ *  Read signal inputs from a YAML file.
+ *
+ *  Example
+ *  -------
+ *      ---
+ *      kind: Input
+ *      metadata:
+ *        name: hym_inst
+ *        annotations:
+ *          default_channel: test  # Search by name (in this case).
+ *      spec:
+ *        samples:
+ *          - sample: 0.0005
+ *            values:
+ *              - D_BrakePedal_0_1_f: 0.0  # 0..1
+ *              - D_ActMode_1_3_f: 3  # 1 = MC Press, 2 = Pedal Force, 3 = Rod Stroke
+ *          - sample: 0.0010
+ *            values:
+  *             - D_BrakePedal_0_1_f: 0.3
+ */
+typedef struct ValueObject {
+    const char* signal;
+    double      value;
+} ValueObject;
+
+typedef struct SampleObject {
+    double  sample_time;
+    HashMap values;
+    void*   data;  // Sample YAML node.
+} SampleObject;
+
+typedef struct Enumerator {
+    const char*           path;
+    SchemaObject          object;  // Matched YAML doc (object->doc).
+    SchemaObjectGenerator generator;
+    /* Tracking variables. */
+    uint32_t              index;
+    SampleObject*         sample;
+    SignalVector*         sv;
+} Enumerator;
+
+static void* value_object_generator(ModelInstanceSpec* mi, void* data);
+static void* sample_object_generator(ModelInstanceSpec* mi, void* data);
+static int   setup_enumerator(ModelInstanceSpec* mi, SchemaObject* object);
 
 
 /**
@@ -75,14 +131,61 @@ int main(int argc, char** argv)
     print_signal_vectors(sv);
 
 
+    /* Load any input data. */
+    Enumerator  samples = { .sv = sv };
+    SchemaObjectSelector selector = {
+        .kind = "Input",
+        .name = args.name,
+        .data = &samples,
+    };
+    schema_object_search(mi, &selector, setup_enumerator);
+    if (samples.object.doc) {
+        /* Load the first sample (for following loop). */
+        samples.sample = schema_object_enumerator(mi, &samples.object,
+            samples.path, &samples.index, samples.generator);
+    }
+
+
     /* Run the Model/Simulation. */
     log_notice("Starting Simulation (for %d steps) ...", args.steps);
+    double model_time = 0.0;  // no adapter, so fake it.
     for (uint32_t i = 0; i < args.steps; i++) {
-        log_notice("  step %d", i);
+        log_notice("  step %d (model_time=%f)", i, model_time);
+
+        /* Set any input signals. */
+        if (samples.sample) {
+            while (samples.sample->sample_time <= model_time) {
+                char** keys = hashmap_keys(&samples.sample->values);
+                int    len = hashmap_number_keys(samples.sample->values);
+                for (int i = 0; i < len; i++) {
+                    double* value =
+                        hashmap_get(&samples.sample->values, keys[i]);
+                    for (uint32_t j = 0; j < sv->count; j++) {
+                        if (strcmp(keys[i], samples.sv->signal[j]) == 0) {
+                            log_notice("    input: %s=%f", keys[i], *value);
+                            if (samples.sv) samples.sv->scalar[j] = *value;
+                            break;
+                        }
+                    }
+                }
+                for (int i = 0; i < len; i++) free(keys[i]);
+                free(keys);
+                hashmap_destroy(&samples.sample->values);
+                free(samples.sample);
+                // next sample
+                samples.sample = NULL;
+                samples.sample = schema_object_enumerator(mi, &samples.object,
+                    samples.path, &samples.index, samples.generator);
+                if (samples.sample == NULL) break;  // no more samples.
+            }
+        }
+
+        /* Step the model. */
         marshal_to_signal_vectors(sv);
         rc = modelc_step(mi, args.step_size);
         if (rc) log_fatal("Call: modelc_step failed! (i=%d, rc=%d)!", i, rc);
         marshal_from_signal_vectors(sv);
+        model_time += args.step_size;
     }
     log_notice("Simulation complete.");
     print_signal_vectors(sv);
@@ -140,4 +243,78 @@ static void print_signal_vectors(SignalVector* sv)
         /* Next signal vector. */
         sv++;
     }
+}
+
+
+static void* value_object_generator(ModelInstanceSpec* mi, void* data)
+{
+    UNUSED(mi);
+
+    YamlNode* node = data;
+
+    int       len = hashmap_number_keys(node->mapping);
+    if (len == 0) return NULL;
+
+    char**    keys = hashmap_keys(&node->mapping);
+    ValueObject* vo = calloc(1, sizeof(ValueObject));
+    vo->signal = keys[0];
+    dse_yaml_get_double(node, keys[0], &vo->value);
+    /* Free the keys, except for the first entry ... which is returned. */
+    for (int i = 1; i < len; i++) free(keys[i]);
+    free(keys);
+
+    return vo; /* Caller to free, also vo->signal. */
+}
+
+
+static void* sample_object_generator(ModelInstanceSpec* mi, void* data)
+{
+    UNUSED(mi);
+
+    SampleObject* sample_obj = calloc(1, sizeof(SampleObject));
+    dse_yaml_get_double((YamlNode*)data, "sample", &sample_obj->sample_time);
+    sample_obj->data = data;
+    hashmap_init_alt(&sample_obj->values, 10, NULL);
+
+    /* Enumerate over the values. */
+    {
+        ValueObject* vo;
+        SchemaObject so = { .doc = data };
+        uint32_t     index = 0;
+        do {
+            vo = schema_object_enumerator(
+                mi, &so, "values", &index, value_object_generator);
+            if (vo == NULL) break;
+            hashmap_set_double(&sample_obj->values, vo->signal, vo->value);
+            if (vo->signal) free(vo->signal);
+            free(vo);
+        } while (1);
+    }
+
+    return sample_obj; /* Caller to free. */
+}
+
+
+static int setup_enumerator(ModelInstanceSpec* mi, SchemaObject* object)
+{
+    UNUSED(mi);
+
+    Enumerator* samples = object->data;
+    samples->object.doc = object->doc;
+    samples->path = "spec/samples";
+    samples->index = 0;
+    samples->sample = NULL;
+    samples->generator = sample_object_generator;
+
+    /* Search for the default signal vector. */
+    const char* name = NULL;
+    dse_yaml_get_string(
+        object->doc, "metadata/annotations/default_channel", &name);
+    while (samples->sv && samples->sv->name) {
+        if (strcmp(samples->sv->name, name) == 0) break;
+        /* Next sv. */
+        samples->sv++;
+    }
+
+    return 1;  // only use the first match.
 }
